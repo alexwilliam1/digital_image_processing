14cb974a390ba3c204a1239acd79af19fde39ac4
alexwilliam1 <alexwilliam_00@hotmail.com>
2020-02-27 22:09:09 -0300

    Sobel test

--- abcd.py
+++ abcd.py
@@ -1,30 +1,74 @@
 import numpy as np
-import cv2
- 
-# Cargamos la imagen
-original = cv2.imread("/home/alexwilliam/Documentos/TCC/PH2/Segmentadas/MelanomaSegmentada/IMD211_lesion.bmp")
-cv2.imshow("original", original)
- 
-# Convertimos a escala de grises
-gris = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
- 
-# Aplicar suavizado Gaussiano
-gauss = cv2.GaussianBlur(gris, (5,5), 0)
- 
-cv2.imshow("suavizado", gauss)
- 
-# Detectamos los bordes con Canny
-canny = cv2.Canny(gauss, 50, 150)
- 
-cv2.imshow("canny", canny)
- 
-# Buscamos los contornos
-(contornos,_) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
- 
-# Mostramos el número de monedas por consola
-print("He encontrado {} objetos".format(len(contornos)))
- 
-cv2.drawContours(original,contornos,-1,(0,0,255), 2)
-cv2.imshow("contornos", original)
- 
-cv2.waitKey(0)
+import cv2 as cv
+import sys
+
+'''# Let's load a simple image with 3 black squares 
+image = cv.imread('../PH2/Segmentadas/MelanomaSegmentada/IMD285_lesion.bmp') 
+  
+# Grayscale 
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) 
+  
+# Find Canny edges 
+edged = cv.Canny(gray, 30, 200) 
+  
+# Finding Contours 
+# Use a copy of the image e.g. edged.copy() 
+# since findContours alters the image 
+contours, hierarchy = cv.findContours(edged,  
+    cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
+  
+cv.imshow('Canny Edges After Contouring', edged) 
+cv.waitKey(0) 
+  
+print("Number of Contours found = " + str(len(contours))) 
+  
+# Draw all contours 
+# -1 signifies drawing all contours 
+cv.drawContours(image, contours, -1, (0, 255, 0), 3) 
+  
+cv.imshow('Contours', image) 
+cv.waitKey(0) 
+cv.destroyAllWindows() '''
+
+
+def main():
+    
+    window_name = ('Sobel Demo - Simple Edge Detector')
+    scale = 1
+    delta = 0
+    ddepth = cv.CV_16S
+    
+
+    # Load the image
+    src = cv.imread('../PH2/Segmentadas/MelanomaSegmentada/IMD285_lesion.bmp', cv.IMREAD_COLOR)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image: ' + 'IMD285_lesion.bmp')
+        return -1
+    
+    
+    src = cv.GaussianBlur(src, (3, 3), 0)
+    
+    
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    
+    
+    grad_x = cv.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+    # Gradient-Y
+    # grad_y = cv.Scharr(gray,ddepth,0,1)
+    grad_y = cv.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+    
+    
+    abs_grad_x = cv.convertScaleAbs(grad_x)
+    abs_grad_y = cv.convertScaleAbs(grad_y)
+    
+    
+    grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    
+    
+    cv.imshow(window_name, grad)
+    cv.waitKey(0)
+    
+    return 0
+if __name__ == "__main__":
+    main()
