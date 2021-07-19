52939ed714fa5f909c85761660b432bddfe2b09e
alexwilliam1 <alexwilliam_00@hotmail.com>
2021-07-18 21:23:56 -0300

    code corrections

--- LBP - Local Binary Patterns/Basic-3x3-LBP.py
+++ LBP - Local Binary Patterns/Basic-3x3-LBP.py
@@ -4,16 +4,20 @@ from matplotlib import pyplot as plt
 import glob
 from os.path import isfile, join
 
+
 def main():
-    img = read_folder("/home/alex/Documentos/TCC2/PH2Dataset/MelanomaSegmentada/*.bmp")
+    img = read_images(
+        "/home/alex/Documentos/TCC2/PH2Dataset/MelanomaSegmentada/*.bmp")
     for i in range(len(img)):
         img2 = img[i]
-        lbp(img[i],img2,i)
+        lbp(img[i], img2, i)
 
-def read_folder(path):
-    images = [cv2.imread(file,0) for file in glob.glob(path)]
+
+def read_images(path):
+    images = [cv2.imread(file, 0) for file in glob.glob(path)]
 
     return images
+
 
 def thresholded(center, pixels):
     out = []
@@ -24,45 +28,51 @@ def thresholded(center, pixels):
             out.append(0)
     return out
 
+
 def get_pixel_else_0(l, idx, idy, default=0):
     try:
-        return l[idx,idy]
+        return l[idx, idy]
     except IndexError:
         return default
+
 
 def lbp(img, transformed_img, i):
     for x in range(0, len(img)):
         for y in range(0, len(img[0])):
-            center        = img[x,y]
-            top_left      = get_pixel_else_0(img, x-1, y-1)
-            top_up        = get_pixel_else_0(img, x,   y-1)
-            top_right     = get_pixel_else_0(img, x+1, y-1)
-            right         = get_pixel_else_0(img, x+1, y)
-            left          = get_pixel_else_0(img, x-1, y)
-            bottom_left   = get_pixel_else_0(img, x-1, y+1)
-            bottom_right  = get_pixel_else_0(img, x+1, y+1)
-            bottom_down   = get_pixel_else_0(img, x,   y+1)
+            center = img[x, y]
+            top_left = get_pixel_else_0(img, x-1, y-1)
+            top_up = get_pixel_else_0(img, x,   y-1)
+            top_right = get_pixel_else_0(img, x+1, y-1)
+            right = get_pixel_else_0(img, x+1, y)
+            left = get_pixel_else_0(img, x-1, y)
+            bottom_left = get_pixel_else_0(img, x-1, y+1)
+            bottom_right = get_pixel_else_0(img, x+1, y+1)
+            bottom_down = get_pixel_else_0(img, x,   y+1)
 
-            values = thresholded(center, [top_left, top_up, top_right, right, bottom_right, bottom_down, bottom_left, left])
+            values = thresholded(center, [
+                                 top_left, top_up, top_right, right, bottom_right, bottom_down, bottom_left, left])
 
             weights = [1, 2, 4, 8, 16, 32, 64, 128]
             res = 0
             for a in range(0, len(values)):
                 res += weights[a] * values[a]
 
-            transformed_img.itemset((x,y), res)
+            transformed_img.itemset((x, y), res)
 
-        #print(x)
+    Melanoma(transformed_img, i)
+    NoMelanoma(transformed_img, i)
 
-    Melanoma(transformed_img,i)
-    #NaoMelanoma(transformed_img,i)
 
-def Melanoma(img_lbp,i):
-    cv2.imwrite("/home/alex/Área de Trabalho/teste_lbp/ImgLBP_{:>04}.png".format(i),img_lbp)
-    print("PH2 - LBP MELANOMA",i+1)
+def Melanoma(img_lbp, i):
+    cv2.imwrite(
+        "/home/alex/Área de Trabalho/teste_lbp/ImgLBP_{:>04}.png".format(i), img_lbp)
+    print("PH2 - LBP MELANOMA", i+1)
 
-def NaoMelanoma(img_lbp,i):
-    cv2.imwrite("/home/alex/Área de Trabalho/PH2/LBP/NãoMelanoma/ImgLBP_{:>04}.png".format(i),img_lbp)
-    print("PH2 - LBP NAO MELANOMA",i+1)
+
+def NoMelanoma(img_lbp, i):
+    cv2.imwrite(
+        "/home/alex/Área de Trabalho/PH2/LBP/NãoMelanoma/ImgLBP_{:>04}.png".format(i), img_lbp)
+    print("PH2 - LBP NAO MELANOMA", i+1)
+
 
 main()
--- LBP - Local Binary Patterns/LBP.py
+++ LBP - Local Binary Patterns/LBP.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import glob
 from os.path import isfile, join
 
+
 def get_pixel(img, center, x, y):
     new_value = 0
     try:
@@ -13,6 +14,7 @@ def get_pixel(img, center, x, y):
         pass
     return new_value
 
+
 def lbp_calculated_pixel(img, x, y):
     '''
      64 | 128 |   1
@@ -20,7 +22,7 @@ def lbp_calculated_pixel(img, x, y):
      32 |   0 |   2
     ----------------
      16 |   8 |   4    
-    '''    
+    '''
     center = img[x][y]
     val_ar = []
     val_ar.append(get_pixel(img, center, x-1, y+1))     # top_right
@@ -31,12 +33,13 @@ def lbp_calculated_pixel(img, x, y):
     val_ar.append(get_pixel(img, center, x, y-1))       # left
     val_ar.append(get_pixel(img, center, x-1, y-1))     # top_left
     val_ar.append(get_pixel(img, center, x-1, y))       # top
-    
+
     power_val = [1, 2, 4, 8, 16, 32, 64, 128]
     val = 0
     for i in range(len(val_ar)):
         val += val_ar[i] * power_val[i]
-    return val    
+    return val
+
 
 def read_folder(path):
     filenames = glob.glob(path)
@@ -44,26 +47,33 @@ def read_folder(path):
     images = [cv2.imread(file) for file in filenames]
     return images
 
-def Melanoma(img_lbp,i):
-    cv2.imwrite("/home/alex/Área de Trabalho/teste_lbp/ISIC/Melanoma/ImgLBP_{:>03}.png".format(i),img_lbp)
-    print("ISIC - LBP MELANOMA",i+1)
 
-def NaoMelanoma(img_lbp,i):
-    cv2.imwrite("/home/alex/Área de Trabalho/teste_lbp/ISIC/NãoMelanoma/ImgLBP_{:>03}.png".format(i),img_lbp)
-    print("ISIC - LBP NAO MELANOMA",i+1)
+def Melanoma(img_lbp, i):
+    cv2.imwrite(
+        "/home/alex/Área de Trabalho/teste_lbp/ISIC/Melanoma/ImgLBP_{:>03}.png".format(i), img_lbp)
+    print("ISIC - LBP MELANOMA", i+1)
+
+
+def NoMelanoma(img_lbp, i):
+    cv2.imwrite(
+        "/home/alex/Área de Trabalho/teste_lbp/ISIC/NãoMelanoma/ImgLBP_{:>03}.png".format(i), img_lbp)
+    print("ISIC - LBP NAO MELANOMA", i+1)
+
 
 def main():
-    img_bgr = read_folder("/home/alex/Documentos/TCC2/ISIC/NaoMelanomaSegmentada/*.png")
+    img_bgr = read_folder(
+        "/home/alex/Documentos/TCC2/ISIC/NaoMelanomaSegmentada/*.png")
     for cont in range(len(img_bgr)):
         height, width, channel = img_bgr[cont].shape
         img_gray = cv2.cvtColor(img_bgr[cont], cv2.COLOR_BGR2GRAY)
-        
-        img_lbp = np.zeros((height, width,3), np.uint8)
+
+        img_lbp = np.zeros((height, width, 3), np.uint8)
         for i in range(0, height):
             for j in range(0, width):
-                 img_lbp[i, j] = lbp_calculated_pixel(img_gray, i, j)
+                img_lbp[i, j] = lbp_calculated_pixel(img_gray, i, j)
 
-        #Melanoma(img_lbp,cont)
-        NaoMelanoma(img_lbp,cont)
-                                 
+        Melanoma(img_lbp, cont)
+        NoMelanoma(img_lbp, cont)
+
+
 main()
